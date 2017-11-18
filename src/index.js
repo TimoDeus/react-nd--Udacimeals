6fc3fd6c8bed36d965c5a9978f67a09c22e659aa
Timo Deuschle <github@timodeuschle.de>
2017-11-18 10:53:16 +0100

    added middleware

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import reducer from './reducers/index.js';
 import {Provider} from 'react-redux';
+import {logger} from 'redux-logger'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
 	reducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	composeEnhancers(
+		applyMiddleware(logger)
+	)
 );
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 ReactDOM.render(
 	<Provider store={store}>
