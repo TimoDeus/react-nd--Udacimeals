1b5d34055de819140a7483a58da8432c1afd0fa3
Timo Deuschle <github@timodeuschle.de>
2017-11-18 11:16:26 +0100

    added thunk middleware

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import reducer from './reducers/index.js';
 import {Provider} from 'react-redux';
 import {logger} from 'redux-logger'
+import thunk from 'redux-thunk'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
 	reducer,
 	composeEnhancers(
-		applyMiddleware(logger)
+		applyMiddleware(logger, thunk)
 	)
 );
 
